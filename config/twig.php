cff7a34ef7fb1649f3a98236f34eb0dc7d0319b1
Lionel D <lionel.deri@gmail.com>
2023-10-21 11:48:43 +0200

    solution twig 2

--- config/twig.php
+++ config/twig.php
@@ -1,5 +1,7 @@
 <?php
 
+// Ne pas oublier de lancer 'composer install' depuis la racine afin que composer puisse télécharger les librairies via le fichier composer.lock
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
